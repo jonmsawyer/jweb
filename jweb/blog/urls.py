eac153bcb3579b45d5a50229775e570cd49b99fb
Jonathan Sawyer <jonmsawyer@gmail.com>
2015-01-13 01:27:52 +0000

    Troubleshooting...

--- jweb/blog/urls.py
+++ jweb/blog/urls.py
@@ -4,8 +4,9 @@ from blog import views
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^(?P<slug>[a-z0-9-]+)/$', views.blog, name='blog'),
-    url(r'^page/(?P<page>\d+)/$', views.home, name='blog_index_page'),
+    url(r'^page/(?P<page>\d+/)?$', views.home, name='blog_index_page'),
+    url(r'^tag/(?P<tag>[a-z0-9-]+)/$', views.tag, name='tag_index'),
+    url(r'^post/(?P<slug>[a-z0-9-]+)/$', views.blog, name='blog'),
     url(r'^$', views.home, name='blog_index'),
 )
 
--- jweb/blog/views.py
+++ jweb/blog/views.py
@@ -12,8 +12,10 @@ def home(request, page=1):
     cd = {}
     cd = gb(cd, 'Home')
     cd = gb(cd, 'Blog', name='blog_index')
+    
     blogs_list = Blog.objects.order_by('-created_at')
     paginator = Paginator(blogs_list, 10) # Show 25 blogs per page
+    
     try:
         blogs = paginator.page(page)
     except PageNotAnInteger:
@@ -29,22 +31,39 @@ def home(request, page=1):
             blog.preview = ''.join(blog.content.split('<!--preview-->')[1]).split('<!--/preview-->')[0]
         except IndexError:
             blog.preview = blog.content
+    
     cd['blogs'] = blogs
     cd['page_range'] = paginator.page_range
     cd['current_page_num'] = blogs.number
     cd['blog_active'] = 'active'
+    
     return render(request, 'blog/home.html', cd)
 
 def blog(request, slug):
     cd = {}
     cd = gb(cd, 'Home')
     cd = gb(cd, 'Blog', name='blog_index')
-
+    
     blog = Blog.objects.get(slug=slug)
-
-    cd['blogs'] = (blog,)
-    for blog in cd['blogs']:
-        blog.list_tags = blog.tags.split()
+    blog.list_tags = blog.tags.split()
+    
     cd['blog_active'] = 'active'
+    cd['blogs'] = (blog,)
+
     return render(request, 'blog/blog.html', cd)
 
+def tag(request, tag):
+    cd = {}
+    cd = gb(cd, 'Home')
+    cd = gb(cd, 'Blog', name='blog_index')
+    cd = gb(cd, 'Tag', name='tag_index')
+
+    if tag:
+        blogs_list = Blog.objects.filter(tag__like=tag).order_by('-created_at')
+    else:
+        blogs_list = Blog.objects.order_by('-created_at')
+
+    cd['blogs'] = blogs_list
+    cd['blog_active'] = 'active'
+
+    return render(request, 'blog/home.html', cd)
